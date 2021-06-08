8db54259722012b5f000b92a3bf3e5e5d49da97e
JMColl <74689077+JMColl@users.noreply.github.com>
2021-06-08 17:53:06 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Colegio la recoleta</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="CSS/estilo.css">
+</head>
+<body>
+<!-- CABECERA-->
+<header class="header">
+	<!-- CONTENEDOR CABECERA-->
+	<div class="container logo-nav-container">
+		<!-- IMAGEN-->
+		<img src="img/logo.jpg" class="logo" width="120" height="90">
+		<!-- MENU-->
+        <nav class="navegacion navbar-expand-sm ">
+        	<ul class="navbar-nav ">
+        		<li class="nav-item navbar-brand"> <a href="index.php" class="nav-link">INICIO</a></li>
+        		<li class="nav-item navbar-brand"> <a href="nosotros.php" class="nav-link">NOSOTROS</a></li>
+        		<li class="nav-item navbar-brand"> <a href="nivelacademico.php" class="nav-link">NIVEL ACADEMICO</a></li>
+        		<li class="nav-item navbar-brand"> <a href="admision.php" class="nav-link">ADMISION</a></li>
+        		<li class="nav-item navbar-brand"> <a href="contactenos.php" class="nav-link">CONTACTANOS</a></li>
+        		<!-- DROPDOWN-->
+        		<li class=" navbar-brand"> <a href="#" class="nav-link">REGISTROS</a>
+        			<ul>
+        				<li><a href="docente.php">DOCENTE</a></li>
+        				<li><a href="alumno.php">ALUMNO</a></li>
+        			</ul>
+        		</li>
+        		<!-- FIN DROPDOWN-->
+        		<li class="nav-item navbar-brand"> <a href="https://www.facebook.com/UTP.Peru" class="nav-link" target="blank"><img src="img/facebook.png" width="45" height="40"></a></li>
+        		 <li class="nav-item navbar-brand"> <a href="https://www.google.es/maps/place/UTP+Torre+Arequipa/@-12.0660981,-77.0395577,17z/data=!3m1!4b1!4m5!3m4!1s0x9105c8ea52a218f9:0x4931f570fd313227!8m2!3d-12.0661034!4d-77.037369" class="nav-link" target="blank"><img src="img/maps.png" width="45" height="40"></a></li>
+        	</ul>
+        </nav>
+	</div>
+</header>
+<!-- FIN CABECERA-->
+<!--ESTRUCTURA-->
+<main class="main">
+	<div class="container">
+    <!--TABLA INICIO-->
+    <table>
+   	<!--Fila1-->
+   	<tr>
+   		<td>
+   			<h1>¡BIENVENIDOS!</h1>
+   			<br><br>
+   			<p>
+   				Una mirada integral y moderna a la educación requiere de una formación sólida en criterios y destrezas, dirigida a cada persona y sustentada en los principios y valores de siempre.Proponemos a nuestros estudiantes un exigente plan de estudios humanista y bilingüe desarrollado en un ambiente de trabajo marcado por la amistad y la colaboración, y orientado a promover su crecimiento personal, el desarrollo de destrezas científicas y la adquisición de habilidades de comunicación y liderazgo social.
+   			</p>
+   		</td>
+   		<td>
+   			<img src="img/alumnosutp.jpg" height="345" width="550">
+   		</td>
+   	</tr>
+   	<!--Fila2-->  
+   	<tr>
+   		<td colspan="2">
+   			<h1>QUIENES SOMOS</h1>
+   	</tr>
+   	<!--Fila3-->
+   	<tr>
+   		<td colspan="2" >
+   			<p>
+   				En Colegio la Recoleta de los Olivos, somos una gran familia, una comunidad educativa de padres, maestros y alumnos que busca el crecimiento personal de cada uno de sus miembros y la transformación progresiva de la sociedad a través de la educación. Lo que buscamos es brindar una sólida formación para que cada uno pueda desplegarse como persona y como profesional en los distintos aspectos de su vida: el estudio, los deportes, el arte, la vida social y la vida espiritual. <br><br>
+   				Es una Institución educativa que atiende en los tres niveles educativos: Inicial, Primaria y Secundaria brindando una atención pedagógica pertinente y oportuna a fin de dotarlos de todas las estrategias necesarias para realizar un proceso de aprendizaje adecuado, dentro de un marco integral, ya que su formación personal se fortalece con la práctica de valores dentro del que hacer educativo.
+   			</p>
+   		</td>
+   	</tr>
+   	<!--Fila3-->
+   	<tr>
+   		<td colspan="2">
+   			<h1>SERVICIOS</h1>
+   	</tr>
+   	<!--Fila4-->
+   	<tr>
+   		<td colspan="2">
+   			<!--Tabla servicios-->
+   			<table >
+   				<!--Fila1-->
+   				<tr>
+   					<td  class="tabser">
+   						<p class="servicios"><b>■ Laboratorios</b></p>
+   						<p>
+   							Moderno laboratorio de Quìmica y Física con microscopios computarizados.Disponemos de una sala de Computo, con acceso a internet, tecnología Core I5 con pantallas LED, Servidor IBM. El internet permite a los alumnos realizar búsquedas y disponer de toda la información que brinda la red.
+   						</p>
+   					</td>
+   					<td class="tabser">
+   						<img src="img/laboratorio.jpg" height="180" width="400" >
+   					</td>
+   				</tr>
+   				<!--Fila2-->
+   				<tr>
+   					<td class="tabser">
+   						<p class="servicios"><b>■ Aulas audiovisuales</b></p>
+   						<p>
+   							Aulas Seguras, ventiladas y amplias aulas, cada una equipada con multimedia y/o con TV. Led de 42, Blue Ray, Grabadoras etc. Con acceso a internet. Todo conlleva a una excelente Educación Audiovisual
+   						</p>
+   					<td class="tabser">
+   						<img src="img/audiovisual.jpg" height="180" width="400" >
+   					</td>
+   				</tr>
+   				<!--Fila3-->
+   				<tr>
+   					<td class="tabser" >
+   						<p class="servicios"><b>■ Biblioteca</b></p>
+   						<p>
+   							Brinda el servicio de préstamo de libros para lectura en sala. Cuenta con un ambiente especial para la lectura de niños. También contamos con un repertorio de libros sobre familia para los niños. <br><br><br>
+   						</p>
+   					</td >
+   					<td class="tabser" align="right">
+   						<img src="img/biblioteca.jpg" height="180" width="400" >
+   					</td>
+   				</tr>
+   			</table>
+   		</td>
+   	</tr>
+   </table>
+   <br>	
+	</div>
+</main>
+<footer class="footer">
+	<div class="container">
+		<font> UTP 2021 | Desarrollodo por NG</font>
+	</div>
+</footer>
+</body>
+</html>
